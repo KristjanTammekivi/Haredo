c6bef3e9124116e3af53eac9191118e694dad8dd
Kristjan Tammekivi <kristjantammekivi@gmail.com>
2019-05-26 19:02:23 +0300

    feat: add .npmignore

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
-import 'source-map-support/register';
-
 export { Queue, QueueOptions } from './queue';
-export { Exchange, ExchangeType, XDelayedType, ExchangeOptions } from './exchange';
+export { Exchange, ExchangeType, XDelayedType, ExchangeOptions, xDelayedTypeStrings } from './exchange';
 export { Haredo, HaredoOptions } from './haredo';
 export { HaredoChain, AddExchange, HaredoChainState } from './haredo-chain';
-export { HaredoError, BadArgumentsError, ChannelBrokenError } from './errors';
-export { PreparedMessage } from './prepared-message';
+export { HaredoError, BadArgumentsError, ChannelBrokenError, MessageAlreadyHandledError } from './errors';
+export { PreparedMessage, ExtendedPublishType, PreparedMessageOptions } from './prepared-message';
 export { HaredoMessage } from './haredo-message';
-export { Consumer } from './consumer';
-export { ConsumerManager, ConsumerManagerEvents } from './consumer-manager';
-export { MessageManager, MessageManagerEvents } from './message-manager';
+export { Consumer, ConsumerOpts, FailHandlerOpts, MessageCallback } from './consumer';
+export { ConsumerManager } from './consumer-manager';
+export { MessageManager } from './message-manager';
