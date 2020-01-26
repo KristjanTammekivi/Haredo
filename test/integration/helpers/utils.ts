11d099bb13de55ae85fa16722b1db4ea324167ec
Kristjan Tammekivi <kristjantammekivi@gmail.com>
2020-01-26 11:31:50 +0200

    test: remove a unnecessary test export

--- test/integration/helpers/utils.ts
+++ test/integration/helpers/utils.ts
@@ -1,6 +1,5 @@
 import { EventEmitter } from 'events';
 import { getChannel } from './amqp';
-import { LogItem } from '../../../src/haredo';
 
 export const eventToPromise = (emitter: EventEmitter, event: string) => {
     return new Promise((resolve) => {
@@ -22,20 +21,4 @@ export const isConsumerClosed = async (queue: string) => {
         throw e;
     }
     return true;
-};
-
-export const debugLogger = (log: LogItem) => console.log(`${log.timestamp.toISOString()} ${log.level} ${log.component}: ${ formatMessage(log.msg)}`);
-const formatMessage = (msg: any[]) => {
-    return msg
-        .map((x) => {
-            if (x instanceof Error) {
-                // tslint:disable-next-line: prefer-template
-                return x.message + '\n' + x.stack;
-            }
-            if (typeof x === 'object') {
-                return JSON.stringify('x');
-            }
-            return x.toString();
-        })
-        .join(', ');
 };
