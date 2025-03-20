9f66d7886050cfb8cb4173f79bc8fd6194d88c1f
Kristjan Tammekivi <kristjantammekivi@gmail.com>
2025-03-20 14:07:53 +0200

    fix: export Queue and Exchange

--- packages/haredo/src/index.ts
+++ packages/haredo/src/index.ts
@@ -4,3 +4,5 @@ export * from './errors';
 export { LogFunction, LogMessage, LogLevel } from './utils/logger';
 export { Middleware } from './utils/apply-middleware';
 export { Extension, ExtensionInterface } from './types';
+export { Exchange } from './exchange';
+export { Queue } from './queue';
