98448fffd278d921b52743517ed09f593d18b60f
Kristjan Tammekivi <kristjantammekivi@gmail.com>
2025-04-30 16:19:14 +0300

    docs: add example for anonymous queues

---
+++ packages/haredo/src/examples/anonymous-queue.ts
@@ -0,0 +1,24 @@
+import { Haredo } from '../haredo';
+import { delay } from '../utils/delay';
+
+const start = async () => {
+    const haredo = Haredo({ url: process.env.RABBIT_URL || 'amqp://localhost' });
+    await haredo.connect();
+
+    await haredo
+        .queue<{ id: number }>('')
+        .bindExchange('testExchange', ['message.new', 'message.updated'], 'topic')
+        .subscribe(async (data, { routingKey }) => {
+            console.log(new Date(), 'Message created or updated:', data.id, routingKey);
+        });
+
+    let iteration = 0;
+    const routingKeys = ['message.new', 'message.updated', 'message.deleted'];
+    while (true) {
+        const routingKey = routingKeys[iteration % routingKeys.length];
+        await haredo.exchange<{ id: number }>('testExchange', 'topic').publish({ id: ++iteration }, routingKey);
+        await delay(1000);
+    }
+};
+
+process.nextTick(start);
